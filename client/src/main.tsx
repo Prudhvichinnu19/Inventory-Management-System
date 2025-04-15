cbd69f86cb267da5cc901e58e92e161ed4172288
Prudhvichinnu19 <prudhvichinnu19@gmail.com>
2025-04-15 10:59:09 +0530

    Delete client directory

--- client/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-// Set the title of the application
-document.title = "Inventory Management System";
-
-createRoot(document.getElementById("root")!).render(<App />);
